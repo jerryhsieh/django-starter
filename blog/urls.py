954c00c1242d1cd03e03f2cf3765ccc9ace6c5f4
vagrant <vagrant@node1.onionstudio.com.tw>
2018-08-29 07:40:37 +0000

    Part 2

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('details/<int:id>', views.details, name='details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+from .models import Post
+
+posts = Post.objects.all()[:10]
+
+context = {
+    'title': 'Latest Posts',
+    'posts': posts
+}
 
 
 def index(request):
-    return HttpResponse('Hello from Blog')
+    #    return HttpResponse('Hello from Blog')
+    print(posts)
+    return render(request, 'blog/index.html', context)
+
+
+def details(request, id):
+    post = Post.objects.get(id=id)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/details.html', context)
